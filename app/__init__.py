bc9aff92155d6fef2707763e772ed9e3502542e2
KarlWithK <cjh16@rice.edu>
2022-06-05 22:57:08 -0500

    Made all the data we are going to pass

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask import url_for, redirect
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 from os import urandom
+from typing import List
+import json
+import pickle
 
 load_dotenv()
 UPLOAD_FOLDER = "./app/static/img/"
@@ -12,6 +15,24 @@ app.secret_key = urandom(32)  # random 32 bit key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+class Data():
+    def __init__(self, first_name, last_name, summary, email, work_exp, hobbies,
+            education, impression, song, platform, filename, title):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.summary: str = summary
+        self.email: str = email
+        self.work_exp: List[str] = work_exp
+        self.hobby: List[str] = hobbies
+        self.education: List[str] = education
+        self.impression: str = impression
+        self.song: str = song
+        self.platform: str = platform
+        self.filename: str = filename
+        self.title: str = title
+
 
 
 def allowed_file(filename):
@@ -26,8 +47,56 @@ def not_found(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Homepage")
 
+@app.route('/karl')
+def karl():
+    first_name = "Karl"
+    last_name = "Hernandez"
+    summary = ""
+    email = "cjh16@rice.edu"
+    work_exp = []
+    hobbies = []
+    education = []
+    impression = ""
+    song = ""
+    platform = ""
+    filename = "signal-2022-05-31-163344_001.jpeg"
+    title = first_name + " " + last_name
+    karl = Data(first_name, last_name, summary, email, work_exp, hobbies, education, impression, song, platform, filename, title)
+    pickled_karl = pickle.dumps(karl)
+    session['current_user'] = pickled_karl
+    krl = pickle.loads(session['current_user'])
+
+    return render_template('portfolio.html', fname=first_name, lname=last_name,
+                           summary=summary, experience="", email=email,
+                           hobby="none",
+                           impression=impression, education="", song=song,
+                           platform=platform, title="Karl Hernandez",
+                           pic_path=filename)
+
+@app.route('/joaquin')
+def joaquin():
+    first_name = "Karl"
+    last_name = "Hernandez"
+    summary = ""
+    email = "cjh16@rice.edu"
+    work_exp = ""
+    hobby = ""
+    education = ""
+    impression = ""
+    song = ""
+    platform = ""
+    filename = ""
+    return render_template('portfolio.html', fname=first_name, lname=last_name,
+                           summary=summary, experience=work_exp, email=email, hobby=hobby,
+                           impression=impression, education=education, song=song,
+                           platform=platform, title="Karl Hernandez",
+                           pic_path=filename)
+
+@app.route('/form')
+def form():
+    return render_template('form.html', title="Create Your Own Portfolio")
 
 @app.route('/portfolio', methods=["POST"])
 def portfolio():
@@ -56,5 +125,25 @@ def portfolio():
                            platform=platform, pic_path=filename, title=title)
 
 
+@app.route('/portfolio')
+def hobby():
+    return "hobbyies"
+
+
 if __name__ == "__main__":
+    first_name = "Karl"
+    last_name = "Hernandez"
+    summary = ""
+    email = "cjh16@rice.edu"
+    work_exp = []
+    hobbies = []
+    education = []
+    impression = ""
+    song = ""
+    platform = ""
+    filename = "signal-2022-05-31-163344_001.jpeg"
+    title = first_name + " " + last_name
+    what = Data(first_name, last_name, summary, email, work_exp, hobbies, education, impression, song, platform, filename, title)
+    print(what.__dict__)
+    print(json.dumps(what.__dict__))
     app.run(debug=True)
