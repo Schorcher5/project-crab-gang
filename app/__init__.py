a0fbf19a2efd759da25a7af7aab954fe15193095
Schorcher5 <2014joaquincisneros@gmail.com>
2022-06-25 02:15:23 +0000

    connected app to sql server

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ from os import urandom
 from typing import List
 from google_images_search import GoogleImagesSearch
 import pickle
+from peewee import *
 
 config = load_dotenv("example.env")
 UPLOAD_FOLDER = "./app/static/img/"
 app = Flask(__name__)
+mydb =  MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
 app.secret_key = urandom(32)  # random 32 bit key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
